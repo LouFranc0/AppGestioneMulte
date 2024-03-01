44114dcd2314550a7f77bd5441ffcaf4e91cb73b
Franncesco Caputo <caputo.francesco001@gmail.com>
2024-03-01 17:09:40 +0100

    FixContestaVerbali

--- obj/Debug/net8.0/S5-ProgettoPolizia.AssemblyInfo.cs
+++ obj/Debug/net8.0/S5-ProgettoPolizia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("S5-ProgettoPolizia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ebb1b281b31e63a6361cd97f696a4b73da8aec1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6679d244680b1c62186349c2392ee11f7a27a032")]
 [assembly: System.Reflection.AssemblyProductAttribute("S5-ProgettoPolizia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("S5-ProgettoPolizia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
