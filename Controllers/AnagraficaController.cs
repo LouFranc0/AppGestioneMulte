505246c046751637d80877599f792a533ddca2e1
Franncesco Caputo <caputo.francesco001@gmail.com>
2024-03-01 14:06:50 +0100

    FIXANAGRAFICA

--- Controllers/AnagraficaController.cs
+++ Controllers/AnagraficaController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
 using S5_ProgettoPolizia.Models;
 using static System.Net.Mime.MediaTypeNames;
 
@@ -9,13 +10,39 @@ namespace S5_ProgettoPolizia.Controllers
     {
         private string connString = "Server=localhost,1433;Database=ESERCIZIOS2L5; User Id=sa;Password=NotHunter2 Initial Catalog=S5-ProgettoPolizia; Integrated Security=true; TrustServerCertificate=True";
 
-        public IActionResult Anagrafica()
+        public IActionResult Add()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Anagrafica(string surname, string name, string address, string city, int CAP, string CodiceFiscale)
+        public IActionResult Add(AnagraficaModel dati)
+        {
+            var conn = new SqlConnection(connString);
+            try
+            {
+                conn.Open();
+                var cmd = new SqlCommand("INSERT INTO Anagrafica (Cognome, Nome, Indirizzo, Città, CAP, CodiceFiscale) VALUES (@surname, @name, @address, @city, @CAP, @CodiceFiscale)", conn);
+                cmd.Parameters.AddWithValue("@surname", dati.Cognome);
+                cmd.Parameters.AddWithValue("@name", dati.Nome);
+                cmd.Parameters.AddWithValue("@address", dati.Indirizzo);
+                cmd.Parameters.AddWithValue("@city", dati.Città);
+                cmd.Parameters.AddWithValue("@CAP", dati.CAP);
+                cmd.Parameters.AddWithValue("@CodiceFiscale", dati.Codice_Fiscale);
+                cmd.ExecuteNonQuery();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
+            finally { conn.Close(); }
+
+        }
+    }
+}
+
+/* public IActionResult Anagrafica(string surname, string name, string address, string city, int CAP, string CodiceFiscale)
         {
             var conn = new SqlConnection(connString);
             try
@@ -37,5 +64,5 @@ namespace S5_ProgettoPolizia.Controllers
             }
             finally { conn.Close(); }
         }
-    }
-}
+    */
+
